83130990285e59924601717e2aac90f9cb1d230a
Sanya <sanyayaatiwari@gmail.com>
2024-02-29 00:32:27 +0530

    ui completed

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,21 +1,27 @@
-import Sidebar from "../ui/dashboard/sidebar/sidebar"
-import Navbar from "../ui/dashboard/navbar/navbar"
-import Footer from "../ui/dashboard/footer/footer"
-import styles from "../ui/dashboard/dashboard.module.css"
+import { cards } from "../lib/data";
+import Card from "../ui/dashboard/card/card";
+import Chart from "../ui/dashboard/chart/chart";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Rightbar from "../ui/dashboard/rightbar/rightbar";
+import Transactions from "../ui/dashboard/transactions/transactions";
 
-const Layout = ({children}) => {
+const Dashboard = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.menu}>
-        <Sidebar />
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {cards.map((item) => (
+            <Card item={item} key={item.id} />
+          ))}
+        </div>
+        <Transactions />
+        <Chart />
       </div>
-      <div className={styles.content}>
-        <Navbar />
-        {children}
-        <Footer />
+      <div className={styles.side}>
+        <Rightbar />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Dashboard;
